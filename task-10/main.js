25de25ba17a3cf2d370ca8482468449983dea722
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-08 20:32:51 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -1,9 +1,29 @@
-const prompt = require('prompt');
-prompt.start();
+let str = 'I am programming on Javascript.';
 
+let first = 0
+let last = 0
+for (let i = 0; i < str.length; i++) {
+    if (str[i]==' ') {
+        first = i
+        break
+    }
+    else {
+        first = -1
+    }
+}
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for (let i = str.length - 1; i > 0; i--) {
+    if (str[i]==' ') {
+        last = i
+        break
+    }
+    else {
+        last = -1
+    }
+}
+if (first == -1) {
+    console.log(-1)
+} else {
+    console.log(first);
+    console.log(last);
+}
