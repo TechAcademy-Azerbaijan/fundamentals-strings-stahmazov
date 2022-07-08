8ac3d433f392d19d02aaac68a76b8c33121ecdae
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-08 19:35:52 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,19 @@
-const prompt = require('prompt');
-prompt.start();
+let str = '255222';
+let count2 = 0
+let count5 = 0
 
+for (let i of str) {
+    if (i == '2') {
+        count2++
+    } else {
+        count5++
+    }
+}
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+if (count2 > count5) {
+    console.log('2')
+} else if (count5 > count2) {
+    console.log('5')
+} else {
+    console.log('=')
+}
