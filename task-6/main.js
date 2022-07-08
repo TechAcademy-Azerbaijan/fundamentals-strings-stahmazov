f7bdda2850fae885ce178e663f54abf220c22a7d
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-08 19:56:39 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,11 @@
-const prompt = require('prompt');
-prompt.start();
+let str = 'Hello world! Hello,    country!';
 
+let arr = str.split(' ')
+let count = 0
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for (let i of arr) {
+    if (i.length > 1) {
+        count++
+    }
+}
+console.log(count)
