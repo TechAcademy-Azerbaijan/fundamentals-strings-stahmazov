033a0274e72b598bdb9689e63d7d1dd249df8480
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-08 19:27:08 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -5,5 +5,21 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
     // Write code here
+  let str = result
+
+  let arr = str.split(',');
+
+  for (let i = 0; i < arr.length; i++){
+      arr[i] = parseInt(arr[i])
+  }
+
+  let min = arr[0]
+
+  for (let i of arr) {
+      if (i < min) {
+          min = i
+      }
+  }
+  console.log(min)
   
 });
