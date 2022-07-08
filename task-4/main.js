78e5f05c0a32ca91394d1bc2c2aff4824e42d69d
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-08 20:47:35 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,30 @@
-const prompt = require('prompt');
-prompt.start();
+let str = 'Hello world! I learn to codegge'
+
+let arr_ayiran = ['!', '.', '?']
+let last_simvol_index = 0
+
+for (let i = str.length-1; i > 0; i--){
+    if ((str[i] == '!') | (str[i] == '.') | (str[i] == '?')) {
+        last_simvol_index = i
+        break
+    }
+}
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+if (last_simvol_index === str.length - 1) {
+    let cumle_sayi = 0
+    for (let i = 0; i < str.length; i++) {
+        if ((str[i] == '!') | (str[i] == '.') | (str[i] == '?')) {
+            cumle_sayi++
+        }
+    }
+    console.log(cumle_sayi)
+} else {
+    let cumle_sayi = 1
+    for (let i = 0; i < str.length; i++) {
+        if ((str[i] == '!') | (str[i] == '.') | (str[i] == '?')) {
+            cumle_sayi++
+        }
+    }
+    console.log(cumle_sayi)
+}
