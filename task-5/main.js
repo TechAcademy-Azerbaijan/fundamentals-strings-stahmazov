fc279c47c1457a0728fcec41c6136e97aaedeab0
Said Tahmazov <97827641+stahmazov@users.noreply.github.com>
2022-07-08 19:52:30 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,13 @@
-const prompt = require('prompt');
-prompt.start();
+let str = '+5-2+4-m/n*2:4';
 
+let emeller = ['+', '-', '*']
+let count = 0
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for (let i of str) {
+    for (let j of emeller) {
+        if (i == j) {
+            count++
+        }
+    }
+}
+console.log(count)
